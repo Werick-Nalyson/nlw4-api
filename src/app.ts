69484485b4cf3a4b5df9a698a32523d3ff0c80a3
Werick Nalyson <wericknalyson@gmail.com>
2021-02-24 09:50:40 -0300

    add model and user controller

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import "reflect-metadata"
 import Connection from './typeorm'
 
 import express from 'express'
+import routes from './routes'
+
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 Connection.create().catch(err => console.log(err))
 
