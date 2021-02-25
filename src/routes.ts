53565a5a72137a3a3639a50719f046595de6f075
Werick Nalyson <wericknalyson@gmail.com>
2021-02-25 17:30:45 -0300

    add survey controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import SurveyController from './controllers/SurveyController'
 import userController from './controllers/UserController'
 
 const router = Router()
 
 router.post('/users', userController.create)
+
+router.post('/surveys', SurveyController.create)
+router.get('/surveys', SurveyController.findAll)
 
 export default router
